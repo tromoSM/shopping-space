bbea6fa0c219f99aeecadf1ae79b39e2a03728bc
manindu08 <manindudamsara@gmail.com>
2026-08-15 18:20:51 +0530

    items

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 window.addEventListener('DOMContentLoaded',function(){
     document.querySelectorAll('[contact]').forEach(contact=>{
         contact.addEventListener('click',function(){
-            alert('oka oya tharamatama obanna epa yako')
+            alert('test')
         })
         fetch('inventory/items.json').then(txt=>txt.json()).then(items=>{
             items.items.forEach(item=>{
-                console.log(`${item.name} : ${item.description}`)
+                document.querySelectorAll('items').forEach(itemA=>{
+                    let itemd=document.createElement('item')
+                    let im=document.createElement('img')
+                    im.src=item.images[0]
+                    let name=document.createElement('p')
+                    name.innerText=item.name
+
+                    itemd.append(im,name)
+                    itemA.append(itemd)
+                })
             })
         })
     })
